9838414b7c6a9dc57bfddeb15e3938b8b8a8e5a3
Jiajun <75730859+T-rvw@users.noreply.github.com>
2022-10-09 09:35:07 +0800

    update submodule (#6)

--- Projects/SponzaBaseScene/Source/Main.cpp
+++ Projects/SponzaBaseScene/Source/Main.cpp
@@ -5,7 +5,7 @@ static engine::Engine* s_pEngine = nullptr;
 int main()
 {
 	s_pEngine = new engine::Engine();
-	s_pEngine->InitPlatformWindow("SponzarBaseScene", 800, 600);
+	s_pEngine->InitPlatformWindow("SponzarBaseScene", 1200, 900);
 	s_pEngine->Init();
 	s_pEngine->MainLoop();
 	s_pEngine->Shutdown();
