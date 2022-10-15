d35bdb149ee86f7243212b8dc9054374511425a6
ChaoSXDemon <ChaoSXDemon@gmail.com>
2022-10-14 23:25:46 -0700

    Created FlybyCamera class that deals with view matrix calculation at the same time allowing users to translate and rotate the camera. Camera class will remain the owner for projection matrix calculation. (#8)

---
+++ Engine/Source/Runtime/FlybyCamera.cpp
@@ -0,0 +1,97 @@
+#include "FlybyCamera.h"
+
+#include <memory>
+
+namespace engine
+{
+
+FlybyCamera::FlybyCamera() 
+	: Camera()
+	, m_position(0.0f, 0.0f, 0.0f)
+	, m_orientation(bx::init::Identity)
+{
+	memset(m_viewMatrix, 0, 16 * sizeof(float));
+}
+
+void FlybyCamera::Translate(const bx::Vec3& v)
+{
+	m_position = bx::add(m_position, bx::mul(v, m_orientation));
+	Dirty();
+}
+
+void FlybyCamera::Translate(const float x, const float y, const float z)
+{
+	Translate(bx::Vec3(x, y, z));
+}
+
+void FlybyCamera::Rotate(const bx::Vec3& axis, const float angleDegrees)
+{
+	m_orientation = bx::normalize(bx::fromAxisAngle(axis, angleDegrees));
+	Dirty();
+}
+
+void FlybyCamera::Rotate(const float x, const float y, const float z, const float angleDegrees)
+{
+	Rotate(bx::Vec3(x, y, z), angleDegrees);
+}
+
+void FlybyCamera::Yaw(const float angleDegrees)
+{
+	Rotate(0.0f, 1.0f, 0.0f, angleDegrees);
+}
+
+void FlybyCamera::Pitch(const float angleDegrees)
+{
+	Rotate(1.0f, 0.0f, 0.0f, angleDegrees);
+}
+
+void FlybyCamera::Roll(const float angleDegrees)
+{
+	Rotate(0.0f, 0.0f, 1.0f, angleDegrees);
+}
+
+void FlybyCamera::RotateLocal(const bx::Vec3& axis, const float angleDegrees)
+{
+	const bx::Vec3 rotatedLocalAxis = bx::mul(axis, m_orientation);
+	m_orientation = bx::normalize(bx::fromAxisAngle(rotatedLocalAxis, angleDegrees));
+	Dirty();
+}
+
+void FlybyCamera::RotateLocal(const float x, const float y, const float z, const float angleDegrees)
+{
+	RotateLocal(bx::Vec3(x, y, z), angleDegrees);
+}
+
+void FlybyCamera::YawLocal(const float angleDegrees)
+{
+	RotateLocal(0.0f, 1.0f, 0.0f, angleDegrees);
+}
+
+void FlybyCamera::PitchLocal(const float angleDegrees)
+{
+	RotateLocal(1.0f, 0.0f, 0.0f, angleDegrees);
+}
+
+void FlybyCamera::RollLocal(const float angleDegrees)
+{
+	RotateLocal(0.0f, 0.0f, 1.0f, angleDegrees);
+}
+
+const float* FlybyCamera::GetViewMatrix() const {
+	return m_viewMatrix;
+}
+
+void FlybyCamera::Update()
+{
+	if (!m_dirty) return;
+
+	// Update NDC
+	Camera::Update();
+
+	// Update view matrix
+	bx::mtxFromQuaternion(m_viewMatrix, m_orientation, m_position);
+
+	m_dirty = false;
+}
+
+}	// namespace engine
---
+++ Engine/Source/Runtime/FlybyCamera.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Camera.h"
+
+namespace engine
+{
+
+class FlybyCamera : public Camera
+{
+public:
+	FlybyCamera();
+	~FlybyCamera() = default;
+
+	FlybyCamera(const FlybyCamera&) = delete;
+	FlybyCamera(FlybyCamera&&) = delete;
+	FlybyCamera& operator=(const FlybyCamera&) = delete;
+	FlybyCamera& operator=(FlybyCamera&&) = delete;
+
+	void Translate(const bx::Vec3& v);
+	void Translate(const float x, const float y, const float z);
+	void Rotate(const bx::Vec3& axis, const float angleDegrees);
+	void Rotate(const float x, const float y, const float z, const float angleDegrees);
+	void Yaw(const float angleDegrees);
+	void Pitch(const float angleDegrees);
+	void Roll(const float angleDegrees);
+
+	void RotateLocal(const bx::Vec3& axis, const float angleDegrees);
+	void RotateLocal(const float x, const float y, const float z, const float angleDegrees);
+	void YawLocal(const float angleDegrees);
+	void PitchLocal(const float angleDegrees);
+	void RollLocal(const float angleDegrees);
+
+	const bx::Vec3& GetPosition() const { return m_position; }
+	const bx::Quaternion& GetOrientation() const { return m_orientation; }
+	const float* GetViewMatrix() const;
+
+	void Update() override;
+
+private:
+	bx::Vec3		m_position;
+	bx::Quaternion	m_orientation;
+	float			m_viewMatrix[16];
+};
+
+}	// namespace engine
