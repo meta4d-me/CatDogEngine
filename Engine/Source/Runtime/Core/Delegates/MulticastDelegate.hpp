f8063f5a3dfee639eeec6588379e721f619740c6
T-rvw <429601557@qq.com>
2022-11-07 00:54:51 +0800

    add multicast delegate

---
+++ Engine/Source/Runtime/Core/Delegates/MulticastDelegate.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "Delegate.hpp"
+
+#include <assert.h>
+#include <functional>
+#include <vector>
+
+namespace engine
+{
+
+template<typename T>
+class MulticastDelegate
+{
+private:
+	static_assert("MulticastDelegate class should accept more than one parameter. Void should also pass through.");
+};
+
+template<typename RetVal, typename... Args>
+class MulticastDelegate<RetVal(Args...)>
+{
+public:
+	explicit MulticastDelegate() = default;
+	MulticastDelegate(const MulticastDelegate&) = delete;
+	MulticastDelegate& operator=(const MulticastDelegate&) = delete;
+	MulticastDelegate(MulticastDelegate&&) = default;
+	MulticastDelegate& operator=(MulticastDelegate&&) = default;
+	~MulticastDelegate() = default;
+
+	template<RetVal(*Function)(Args...)>
+	void Bind()
+	{
+		Delegate<RetVal(Args...)> delegate;
+		delegate.Bind<Function>();
+		m_delegates.emplace_back(std::move(delegate));
+	}
+
+	template<typename C, RetVal(C::*Function)(Args...)>
+	void Bind(C* pInstance)
+	{
+		Delegate<RetVal(Args...)> delegate;
+		delegate.Bind<C, Function>(pInstance);
+		m_delegates.emplace_back(std::move(delegate));
+	}
+
+	template<typename C, RetVal(C::*Function)(Args...) const>
+	void Bind(const C* pInstance)
+	{
+		Delegate<RetVal(Args...)> delegate;
+		delegate.Bind<C, Function>(pInstance);
+		m_delegates.emplace_back(std::move(delegate));
+	}
+
+	void Invoke(Args... args) const
+	{
+		assert(!m_delegates.empty() && "MulticastDelegate doesn't bind any function calls.");
+		for(const auto& delegate : m_delegates)
+		{
+			delegate.Invoke(args...);
+		}
+	}
+
+private:
+	std::vector<Delegate<RetVal(Args...)>> m_delegates;
+};
+
+}
