2ea5268e1d3357b9775a8a987783f5abac353d25
t-rvw <429601557@qq.com>
2022-12-04 01:34:30 +0800

    update submodule bat to make it more convenient to use

--- UpdateSubmodules.bat
+++ UpdateSubmodules.bat
@@ -1,8 +1,13 @@
+REM if new submodule added, it can init it from remote.
+git submodule init
+
 REM grab latest commits from server
 git submodule update --recursive --remote
 
-REM above command will set current branch to detached HEAD. set back to master.
-git submodule foreach "git checkout master || git checkout main"
+REM above command will set current branch to detached HEAD. set back to master/main.
+REM docking is a hack for imgui, need a if condition?
+git submodule foreach "git checkout docking || git checkout master || git checkout main"
 
 REM now do pull to fast-forward to latest commit
-git submodule foreach "git pull origin master || git pull origin main"
+REM docking is a hack for imgui, need a if condition?
+git submodule foreach "git pull origin docking || git pull origin master || git pull origin main"
