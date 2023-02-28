81331b2aa391e5f93cab1fc273ae213af8f08926
VTui22 <106135221+VTui22@users.noreply.github.com>
2023-02-28 22:01:20 +0800

    Update Localization.cpp

--- Engine/Source/Runtime/Application/Localization.cpp
+++ Engine/Source/Runtime/Application/Localization.cpp
@@ -8,97 +8,44 @@
 #include<fstream>
 #include <iostream>
 #include "Localization.h"
-#include "ImGui/ImGuiContextInstance.h"
-namespace engine
-{
-	using namespace std;
 
-	vector<string>vec_key;
-	vector<string> vec_CN;
-	vector<string> vec_EN;
-	std::map<std::string, std::pair<std::string, std::string>> texts;
-	const char* keyconst;
-	void Read_csv()
-	{
-		vector<string> scv;
-		std::filesystem::path base_path = std::filesystem::current_path();
-		
-		std::filesystem::path csv_path = base_path / "text_csv" / "text.csv";
-		cout << csv_path << endl;
-
-
-		ifstream fp(csv_path);
-		string line;
-		int textnum = 0;
-		while (getline(fp, line))
-		{
-			stringstream ss(line);
-			string str;
-			while (getline(ss, str, ','))
-			{
-
-				scv.push_back(str + ",");
-
-				textnum++;
-			}
-
-		};
-
-
-
-		string str;
-		str = accumulate(scv.begin(), scv.end(), str);
-		size_t keybegin = str.find("TEXT_TEST");
-		size_t keyend = str.find("TEXT_END") - keybegin;
-		size_t CNbegin = str.find("BEGIN");
-		size_t CNend = (str.find("CNEND") - CNbegin);
-		size_t ENbegin = str.find("Test");
-		size_t ENend = str.find("ENEND") - ENbegin;
-		string keystr = str.substr(keybegin, keyend);
-		string CNstr = str.substr(CNbegin, CNend);
-		string ENstr = str.substr(ENbegin, ENend);
-		stringstream ssCN(CNstr);
-		stringstream sskey(keystr);
-		stringstream ssEN(ENstr);
-		string keybuf;
-		string CNbuf;
-		string ENbuf;
-		int keynum = 0;
-		int CNnum = 0;
-		int ENnum = 0;
-
-		while (getline(sskey, keybuf, ','))
-		{
-			vec_key.push_back(keybuf);
-			keynum++;
-		}
-		while (getline(ssCN, CNbuf, ','))
-		{
-			vec_CN.push_back(CNbuf);
-			CNnum++;
-		}
-		while (getline(ssEN, ENbuf, ','))
-		{
-			vec_EN.push_back(ENbuf);
-			ENnum++;
-		}
-
-		for (std::size_t i = 0; i < vec_key.size(); ++i) {
-			texts[vec_key[i]] = { vec_CN[i], vec_EN[i] };
-		}
-
-
-	}
-	const char* SetText(const char* text)
-	{
-		const char* result;
-		if (1) {
-			result = texts[text].first.c_str();
-		}
-		else {
-			result = texts[text].second.c_str();
-		}
-		return result;
-
-	}
-}
+//namespace engine
+//{// Define global variables
+//    std::map<std::string, std::vector<std::string>> Text_Map;
+//    
+//
+//    // Read data from CSV file and return as a map
+//    void ReadCSV(std::string csv_path) {
+//        std::ifstream file(csv_path);
+//        if (!file.is_open()) {
+//            std::cout << "Failed to open CSV file: " << csv_path << std::endl;
+//            return;
+//        }
+//        std::string line;
+//        while (getline(file, line)) {
+//            std::istringstream ss(line);
+//            std::vector<std::string> words;
+//            std::string word;
+//            while (getline(ss, word, ',')) {
+//                words.push_back(word);
+//            }
+//            std::string key = words[0];
+//            std::string chinese = words[1];
+//            std::string english = words[2];
+//            Text_Map[key] = { chinese, english };
+//        }
+//    }
+//
+//    const char* SetText(std::string key) {
+//        if (Text_Map.find(key) != Text_Map.end()) {
+//            if (1) {
+//                return Text_Map[key][0].c_str();
+//            }
+//            else if (0) {
+//                return Text_Map[key][1].c_str();
+//            }
+//        }
+//        return "Not Found";
+//    }
+//
+//}
