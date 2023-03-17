b1b2c3e479ac73284451376f955eefce4f1195a4
Hinageshi <2247001665@qq.com>
2023-03-17 17:32:40 +0800

    Submit camrea position uniform to shader.

--- Engine/BuiltInShaders/shaders/uniforms.sh
+++ Engine/BuiltInShaders/shaders/uniforms.sh
@@ -1,8 +1,8 @@
+// Temporary file just to render skybox.
+
 uniform vec4 u_params[8];
 
-#define u_mtx0          u_params[0]
-#define u_mtx1          u_params[1]
-#define u_mtx2          u_params[2]
-#define u_mtx3          u_params[3]
-#define u_cameraPos     u_params[4].xyz
-#define u_EV100         u_params[4].w
+#define u_mtx0 u_params[0]
+#define u_mtx1 u_params[1]
+#define u_mtx2 u_params[2]
+#define u_mtx3 u_params[3]
