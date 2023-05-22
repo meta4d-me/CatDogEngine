cf2faf826c2e69059575f5764d478fb51c9a6cd1
t-rvw <429601557@qq.com>
2023-05-23 02:03:04 +0800

    fix build

--- Engine/Auto/Scripts/test.lua
+++ Engine/Auto/Scripts/test.lua
@@ -27,6 +27,8 @@ function MakeTest(testName)
 
 		includedirs {
 			path.join(EngineSourcePath, "Runtime/"),
+			path.join(ThirdPartySourcePath, "AssetPipeline/public"),
+			path.join(EnginePath, "BuiltInShaders/UniformDefines"),
 		}
 
 		-- convenient to test multiple threads
