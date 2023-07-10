321556990fe5c8e97352a379e3d2aed7afb6e21c
Hinageshi <69386319+Hinageshi01@users.noreply.github.com>
2023-07-10 16:01:30 +0800

    Ddgi shader (#199)
    * DDGIComponent store probe num.
    
    * DDGIComponent store volume origin.
    
    * DDGI shader tool functions.
    
    * Sample ddgi irradiance and distance textures.
    
    * Change irradiance texture format to bgfx::TextureFormat::Enum::RGBA16.
    
    * Render ddgi model before blit.
    
    * Apply relocation textre.
    
    * Add U_DDGI.sh
    
    * Update irradiance and distance texture.
    
    * Clamp irradiance by volume size.

--- Engine/Source/Runtime/Rendering/DDGIRenderer.cpp
+++ Engine/Source/Runtime/Rendering/DDGIRenderer.cpp
@@ -9,6 +9,7 @@
 #include "RenderContext.h"
 #include "Rendering/DDGIDefinition.h"
 #include "Scene/Texture.h"
+#include "U_DDGI.sh"
 
 namespace engine
 {
@@ -17,58 +18,56 @@ namespace
 {
 
 constexpr const char* classificationSampler = "s_texClassification";
-constexpr const char* distanceSampler = "s_texDistance";
-constexpr const char* irradianceSampler = "s_texIrradiance";
-constexpr const char* relocationSampler = "s_texRelocation";
+constexpr const char* distanceSampler       = "s_texDistance";
+constexpr const char* irradianceSampler     = "s_texIrradiance";
+constexpr const char* relocationSampler     = "s_texRelocation";
+
+constexpr const char* volumeOrigin          = "u_volumeOrigin";
+constexpr const char* volumeProbeSpacing    = "u_volumeProbeSpacing";
+constexpr const char* volumeProbeCounts     = "u_volumeProbeCounts";
 
 constexpr uint64_t samplerFlags = BGFX_SAMPLER_U_CLAMP | BGFX_SAMPLER_V_CLAMP | BGFX_SAMPLER_W_CLAMP;
 
-static constexpr uint8_t CLASSIFICATICON_GRID_SIZE = 1;
-static constexpr uint8_t DISTANCE_GRID_SIZE = 14 + 2;
-static constexpr uint8_t IRRADIANCE_GRID_SIZE = 6 + 2;
-static constexpr uint8_t RELOCATION_GRID_SIZE = 1;
-
 void CreatDDGITexture(DDGITextureType type, DDGIComponent* pDDGIComponent, RenderContext* pRenderContext)
 {
-	assert(pDDGIComponent);
-	assert(pRenderContext);
-
-	const char* pName = GetDDGITextureTypeName(type);
-	cd::Vec3f probNum = pDDGIComponent->GetDimension() / pDDGIComponent->GetSpacing();
+	assert(nullptr != pDDGIComponent && nullptr != pRenderContext);
 
 	cd::Vec2f textureSize = cd::Vec2f(0.0f, 0.0f);
 	bgfx::TextureFormat::Enum format = bgfx::TextureFormat::Enum::Unknown;
 	const void* data = nullptr;
 	uint32_t dataSize = 0;
+
+	cd::Vec3f probeCount = pDDGIComponent->GetProbeCount();
+
 	switch(type)
 	{
 		case DDGITextureType::Classification:
-			textureSize = cd::Vec2f(probNum.y() * probNum.z(), probNum.x()) * CLASSIFICATICON_GRID_SIZE;
+			textureSize = cd::Vec2f(probeCount.y() * probeCount.z(), probeCount.x()) * CLASSIFICATICON_GRID_SIZE;
 			format = bgfx::TextureFormat::Enum::R32F;
-			data = reinterpret_cast<const void *>(pDDGIComponent->GetClassificationRawData());
+			data = reinterpret_cast<const void*>(pDDGIComponent->GetClassificationRawData());
 			dataSize = pDDGIComponent->GetClassificationSize();
 			break;
 		case DDGITextureType::Distance:
-			textureSize = cd::Vec2f(probNum.y() * probNum.z(), probNum.x()) * DISTANCE_GRID_SIZE;
+			textureSize = cd::Vec2f(probeCount.y() * probeCount.z(), probeCount.x()) * DISTANCE_GRID_SIZE;
 			format = bgfx::TextureFormat::Enum::RG32F;
-			data = reinterpret_cast<const void *>(pDDGIComponent->GetDistanceRawData());
+			data = reinterpret_cast<const void*>(pDDGIComponent->GetDistanceRawData());
 			dataSize = pDDGIComponent->GetDistanceSize();
 			break;
 		case DDGITextureType::Irradiance:
-			textureSize = cd::Vec2f(probNum.y() * probNum.z(), probNum.x()) * IRRADIANCE_GRID_SIZE;
-			// TODO : RGBA16U
+			textureSize = cd::Vec2f(probeCount.y() * probeCount.z(), probeCount.x()) * IRRADIANCE_GRID_SIZE;
 			format = bgfx::TextureFormat::Enum::RGBA16F;
-			data = reinterpret_cast<const void *>(pDDGIComponent->GetIrradianceRawData());
+			data = reinterpret_cast<const void*>(pDDGIComponent->GetIrradianceRawData());
 			dataSize = pDDGIComponent->GetIrradianceSize();
 			break;
 		case DDGITextureType::Relocation:
-			textureSize = cd::Vec2f(probNum.y() * probNum.z(), probNum.x()) * RELOCATION_GRID_SIZE;
+			textureSize = cd::Vec2f(probeCount.y() * probeCount.z(), probeCount.x()) * RELOCATION_GRID_SIZE;
 			format = bgfx::TextureFormat::Enum::RGBA16F;
-			data = reinterpret_cast<const void *>(pDDGIComponent->GetRelocationRawData());
+			data = reinterpret_cast<const void*>(pDDGIComponent->GetRelocationRawData());
 			dataSize = pDDGIComponent->GetRelocationSize();
 			break;
 	}
 
+	const char *pName = GetDDGITextureTypeName(type);
 	if(nullptr != data && dataSize > 0)
 	{
 		pRenderContext->CreateTexture(pName, static_cast<uint16_t>(textureSize.x()), static_cast<uint16_t>(textureSize.y()), 1, format, samplerFlags, data, dataSize);
@@ -81,7 +80,7 @@ void CreatDDGITexture(DDGITextureType type, DDGIComponent* pDDGIComponent, Rende
 
 }
 
-void DDGIRenderer::Init()
+void DDGIRenderer::Init()	
 {
 	assert(m_pCurrentSceneWorld && "Unknown Scene World pointer!");
 	m_pDDGIComponent = m_pCurrentSceneWorld->GetDDGIComponent(m_pCurrentSceneWorld->GetDDGIEntity());
@@ -94,17 +93,25 @@ void DDGIRenderer::Init()
 	m_pRenderContext->CreateUniform(irradianceSampler, bgfx::UniformType::Sampler);
 	m_pRenderContext->CreateUniform(relocationSampler, bgfx::UniformType::Sampler);
 
-	// Temporary code.
+	// TODO : Hard code the centre of current test model(wood room) here.
+	// Warning : The coordinate system is different between CD and HWs Engine.
+	//   CD: Left-hand, +Y Up
+	//   HW: Right-hand, +Z Up
+	m_pDDGIComponent->SetVolumeOrigin(cd::Vec3f(0.0f, 0.0f, 0.0f));
+	m_pDDGIComponent->SetProbeSpacing(cd::Vec3f(2.0f, 2.0f, 2.0f));
+	m_pDDGIComponent->SetProbeCount(cd::Vec3f(4.0f, 2.0f, 5.0f));
 	m_pDDGIComponent->SetAmbientMultiplier(1.0);
-	m_pDDGIComponent->SetDimension(cd::Vec3f(4.0f, 5.0f, 2.0f));
 	m_pDDGIComponent->SetNormalBias(0.0f);
-	m_pDDGIComponent->SetSpacing(cd::Vec3f(1.0f, 1.0f, 1.0f));
 	m_pDDGIComponent->SetViewBias(0.0f);
 
 	m_pDDGIComponent->SetClassificationRawData("ddgi/classification.bin");
 	m_pDDGIComponent->SetDistanceRawData("ddgi/distance.bin");
 	m_pDDGIComponent->SetIrradianceRawData("ddgi/irradiance.bin");
 	m_pDDGIComponent->SetRelocationRawData("ddgi/relocation.bin");
+
+	m_pRenderContext->CreateUniform(volumeOrigin, bgfx::UniformType::Vec4, 1);
+	m_pRenderContext->CreateUniform(volumeProbeSpacing, bgfx::UniformType::Vec4, 1);
+	m_pRenderContext->CreateUniform(volumeProbeCounts, bgfx::UniformType::Vec4, 1);
 
 	CreatDDGITexture(DDGITextureType::Classification, m_pDDGIComponent, m_pRenderContext);
 	CreatDDGITexture(DDGITextureType::Distance, m_pDDGIComponent, m_pRenderContext);
@@ -146,6 +153,7 @@ void DDGIRenderer::Render(float deltaTime)
 		bgfx::setVertexBuffer(0, bgfx::VertexBufferHandle(pMeshComponent->GetVertexBuffer()));
 		bgfx::setIndexBuffer(bgfx::IndexBufferHandle(pMeshComponent->GetIndexBuffer()));
 
+		// Material, only albedo texture will be used for ddgi at now.
 		for(const auto& [textureType, textureInfo] : pMaterialComponent->GetTextureResources())
 		{
 			std::optional<MaterialComponent::TextureInfo> optTextureInfo = pMaterialComponent->GetTextureInfo(textureType);
@@ -165,39 +173,16 @@ void DDGIRenderer::Render(float deltaTime)
 		bgfx::setTexture(4, m_pRenderContext->GetUniform(StringCrc(relocationSampler)),
 						 m_pRenderContext->GetTexture(StringCrc(GetDDGITextureTypeName(DDGITextureType::Relocation))));
 
+		// TODO : Do we need to change the origin at runtime?
+		m_pRenderContext->FillUniform(StringCrc(volumeOrigin), &m_pDDGIComponent->GetVolumeOrigin().x(), 1);
+		m_pRenderContext->FillUniform(StringCrc(volumeProbeSpacing), &m_pDDGIComponent->GetProbeSpacing().x(), 1);
+		m_pRenderContext->FillUniform(StringCrc(volumeProbeCounts), &m_pDDGIComponent->GetProbeCount().x(), 1);
+
 		constexpr uint64_t state = BGFX_STATE_WRITE_MASK | BGFX_STATE_CULL_CCW | BGFX_STATE_MSAA | BGFX_STATE_DEPTH_TEST_LESS;
 		bgfx::setState(state);
 
 		bgfx::submit(GetViewID(), bgfx::ProgramHandle(pMaterialComponent->GetShadingProgram()));
 	}
-}
-
-void DDGIRenderer::UpdateClassificationTexture(const char* path)
-{
-	// m_pRenderContext->Destory(StringCrc(m_pDDGIComponent->GetClassificationTexturePath().c_str()));
-	// m_pRenderContext->CreateTexture(path, samplerFlags);
-	// m_pDDGIComponent->SetClassificationTexturePath(path);
-}
-
-void DDGIRenderer::UpdateDistanceTexture(const char* path)
-{
-	// m_pRenderContext->Destory(StringCrc(m_pDDGIComponent->GetDistanceTexturePath().c_str()));
-	// m_pRenderContext->CreateTexture(path, samplerFlags);
-	// m_pDDGIComponent->SetDistanceTexturePath(path);
-}
-
-void DDGIRenderer::UpdateIrradianceTexture(const char* path)
-{
-	// m_pRenderContext->Destory(StringCrc(m_pDDGIComponent->GetIrradianceTexturePath().c_str()));
-	// m_pRenderContext->CreateTexture(path, samplerFlags);
-	// m_pDDGIComponent->SetIrradianceTexturePath(path);
-}
-
-void DDGIRenderer::UpdateRelocationTexture(const char* path)
-{
-	// m_pRenderContext->Destory(StringCrc(m_pDDGIComponent->GetRelocationTexturePath().c_str()));
-	// m_pRenderContext->CreateTexture(path, samplerFlags);
-	// m_pDDGIComponent->SetRelocationTexturePath(path);
 }
 
 }
