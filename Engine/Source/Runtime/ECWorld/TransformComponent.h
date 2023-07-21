9e5b27392274240fa8179b6f14e4e8ac75f4f5ad
VTui22 <106135221+VTui22@users.noreply.github.com>
2023-07-21 19:27:44 +0800

    Update TransformComponent.h
    Remove commented code

--- Engine/Source/Runtime/ECWorld/TransformComponent.h
+++ Engine/Source/Runtime/ECWorld/TransformComponent.h
@@ -34,18 +34,6 @@ public:
 	void Reset();
 	void Build();
 
-	//Camera's features from transform
-	//cd::Vec3f GetEye() { return this->GetTransform().GetTranslation(); }
-	//void SetEye(cd::Vec3f eye) { this->GetTransform().GetTranslation() = cd::MoveTemp(eye); }
-
-	//cd::Vec3f GetLookAt() { return this->GetTransform().GetRotation().ToMatrix3x3() * cd::Vec3f(0, 0, 1); }
-	//void SetLookAt(cd::Vec3f lookAt) { this->GetTransform().SetRotation(cd::Quaternion::FromAxisAngle(GetLookAt().Cross(lookAt), std::acos(GetLookAt().Dot(lookAt)))); }
-
-	//cd::Vec3f GetUp() { return this->GetTransform().GetRotation().ToMatrix3x3() * cd::Vec3f(0, 1, 0); }
-	//void SetUp(cd::Vec3f up) { this->GetTransform().SetRotation(cd::Quaternion::FromAxisAngle(GetLookAt().Cross(up), std::acos(GetLookAt().Dot(up)))); }
-
-	//cd::Vec3f GetCross() { return this->GetTransform().GetRotation().ToMatrix3x3() * cd::Vec3f(1, 0, 0); }
-	//void SetCross(cd::Vec3f cross) { this->GetTransform().SetRotation(cd::Quaternion::FromAxisAngle(GetLookAt().Cross(cross), std::acos(GetLookAt().Dot(cross)))); }
 #ifdef EDITOR_MODE
 	static bool DoUseUniformScale() { return m_doUseUniformScale; }
 	static void SetUseUniformScale(bool use) { m_doUseUniformScale = use; }
