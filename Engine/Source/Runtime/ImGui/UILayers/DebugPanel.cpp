a36ff0896ee403a5125c388cbec4950a5e57fa74
Hinageshi01 <2247001665@qq.com>
2023-07-31 18:23:01 +0800

    profiler

--- Engine/Source/Runtime/ImGui/UILayers/DebugPanel.cpp
+++ Engine/Source/Runtime/ImGui/UILayers/DebugPanel.cpp
@@ -16,14 +16,14 @@ namespace
 
 struct SampleData
 {
-	static constexpr uint32_t kNumSamples = 100;
+	static constexpr uint32_t SampleNum = 256;
 
 	SampleData()
 	{
-		reset();
+		Reset();
 	}
 
-	void reset()
+	void Reset()
 	{
 		m_offset = 0;
 		bx::memSet(m_values, 0, sizeof(m_values));
@@ -33,18 +33,18 @@ struct SampleData
 		m_avg = 0.0f;
 	}
 
-	void pushSample(float value)
+	void PushSample(float value)
 	{
 		m_values[m_offset] = value;
-		m_offset = (m_offset + 1) % kNumSamples;
+		m_offset = (m_offset + 1) % SampleNum;
 
 		float min = bx::max<float>();
 		float max = bx::min<float>();
 		float avg = 0.0f;
 
-		for (uint32_t ii = 0; ii < kNumSamples; ++ii)
+		for (uint32_t sampleIndex = 0; sampleIndex < SampleNum; ++sampleIndex)
 		{
-			const float val = m_values[ii];
+			const float val = m_values[sampleIndex];
 			min = bx::min(min, val);
 			max = bx::max(max, val);
 			avg += val;
@@ -52,11 +52,11 @@ struct SampleData
 
 		m_min = min;
 		m_max = max;
-		m_avg = avg / kNumSamples;
+		m_avg = avg / SampleNum;
 	}
 
 	int32_t m_offset;
-	float m_values[kNumSamples];
+	float m_values[SampleNum];
 
 	float m_min;
 	float m_max;
@@ -77,6 +77,8 @@ void DebugPanel::Init()
 
 void DebugPanel::Update()
 {
+	ImGui::SetNextWindowSize(ImVec2(350, 200.0f));
+
 	ImGui::Begin(GetName(), &m_isEnable);
 
 	ShowProfiler();
@@ -112,51 +114,50 @@ void DebugPanel::ShowProfiler()
 	static SampleData s_frameTime;
 	
 	const bgfx::Stats* stats = bgfx::getStats();
-	const double toMsCpu = 1000.0 / stats->cpuTimerFreq;
-	const double toMsGpu = 1000.0 / stats->gpuTimerFreq;
-	const double frameMs = double(stats->cpuTimeFrame) * toMsCpu;
+	float toMsCpu = 1000.0f / static_cast<float>(stats->cpuTimerFreq);
+	float toMsGpu = 1000.0f / static_cast<float>(stats->gpuTimerFreq);
+	float frameMs = static_cast<float>(stats->cpuTimeFrame) * toMsCpu;
 
-	s_frameTime.pushSample(float(frameMs));
+	s_frameTime.PushSample(frameMs);
 
-	// char frameTextOverlay[256];
-	// bx::snprintf(frameTextOverlay, BX_COUNTOF(frameTextOverlay), "%s%.3fms, %s%.3fms\nAvg: %.3fms, %.1f FPS"
-	// 	, ICON_FA_ARROW_DOWN
-	// 	, s_frameTime.m_min
-	// 	, ICON_FA_ARROW_UP
-	// 	, s_frameTime.m_max
-	// 	, s_frameTime.m_avg
-	// 	, 1000.0f / s_frameTime.m_avg
-	// );
-	// 
-	// ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImColor(0.0f, 0.5f, 0.15f, 1.0f).Value);
-	// ImGui::PlotHistogram("Frame"
-	// 	, s_frameTime.m_values
-	// 	, SampleData::kNumSamples
-	// 	, s_frameTime.m_offset
-	// 	, frameTextOverlay
-	// 	, 0.0f
-	// 	, 60.0f
-	// 	, ImVec2(0.0f, 45.0f)
-	// );
-	// ImGui::PopStyleColor();
-	// 
+	char frameTextOverlay[256];
+	bx::snprintf(frameTextOverlay, BX_COUNTOF(frameTextOverlay), "%s%.3fms, %s%.3fms\nAvg: %.3fms, %.1f FPS"
+		, ICON_MDI_ARROW_DOWN
+		, s_frameTime.m_min
+		, ICON_MDI_ARROW_UP
+		, s_frameTime.m_max
+		, s_frameTime.m_avg
+		, 1000.0f / s_frameTime.m_avg
+	);
+	
+	ImGui::PushStyleColor(ImGuiCol_PlotHistogram, ImColor(0.0f, 0.5f, 0.15f, 1.0f).Value);
+	ImGui::PlotHistogram("Frame"
+		, s_frameTime.m_values
+		, SampleData::SampleNum
+		, s_frameTime.m_offset
+		, frameTextOverlay
+		, 0.0f
+		, 60.0f
+		, ImVec2(0.0f, 45.0f)
+	);
+	ImGui::PopStyleColor();
+	
 	ImGui::Text("Submit CPU %0.3f, GPU %0.3f (L: %d)"
-		, double(stats->cpuTimeEnd - stats->cpuTimeBegin) * toMsCpu
-		, double(stats->gpuTimeEnd - stats->gpuTimeBegin) * toMsGpu
+		, static_cast<float>(stats->cpuTimeEnd - stats->cpuTimeBegin) * toMsCpu
+		, static_cast<float>(stats->gpuTimeEnd - stats->gpuTimeBegin) * toMsGpu
 		, stats->maxGpuLatency
 	);
-	// 
-	// if (-INT64_MAX != stats->gpuMemoryUsed)
-	// {
-	// 	char tmp0[64];
-	// 	bx::prettify(tmp0, BX_COUNTOF(tmp0), stats->gpuMemoryUsed);
-	// 
-	// 	char tmp1[64];
-	// 	bx::prettify(tmp1, BX_COUNTOF(tmp1), stats->gpuMemoryMax);
-	// 
-	// 	ImGui::Text("GPU mem: %s / %s", tmp0, tmp1);
-	// }
-
+	
+	if (-INT64_MAX != stats->gpuMemoryUsed)
+	{
+		char tmp0[64];
+		bx::prettify(tmp0, BX_COUNTOF(tmp0), stats->gpuMemoryUsed);
+	
+		char tmp1[64];
+		bx::prettify(tmp1, BX_COUNTOF(tmp1), stats->gpuMemoryMax);
+	
+		ImGui::Text("GPU mem: %s / %s", tmp0, tmp1);
+	}
 }
 
 }
