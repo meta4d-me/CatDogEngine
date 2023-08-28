29d151c385ed80edbed5743e5492e3be36cbe59b
刘浩天 <88096545+OVOAOVO@users.noreply.github.com>
2023-08-28 20:59:49 +0800

    splash ui and color picker(#227)

--- Engine/Source/Editor/UILayers/Splash.cpp
+++ Engine/Source/Editor/UILayers/Splash.cpp
@@ -17,26 +17,31 @@ Splash::~Splash()
 
 void Splash::Init()
 {
-	GetRenderContext()->CreateTexture("Textures/lut/ibl_brdf_lut.dds");
+	GetRenderContext()->CreateTexture("Textures/splash_texture.png");
 
 	m_shaderBuildTaskTotalCount = ResourceBuilder::Get().GetCurrentTaskCount();
 }
 
 void Splash::Update()
 {
-	auto flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse;
-	
+	auto flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoTitleBar;
+
 	size_t currentBuildCount = ResourceBuilder::Get().GetCurrentTaskCount();
 	assert(currentBuildCount <= m_shaderBuildTaskTotalCount);
+
 
 	//std::string title = std::format("{}({}/{})", GetName(), m_shaderBuildTaskTotalCount - currentBuildCount, m_shaderBuildTaskTotalCount);
 	std::string title = GetName();
 	title += "(" + std::to_string(m_shaderBuildTaskTotalCount - currentBuildCount) + "/" + std::to_string(m_shaderBuildTaskTotalCount) + ")";
 	ImGui::Begin(title.c_str(), &m_isEnable, flags);
 
-	engine::StringCrc splashTexture("Textures/lut/ibl_brdf_lut.dds");
+	engine::StringCrc splashTexture("Textures/splash_texture.png");
 	bgfx::TextureHandle textureHandle = GetRenderContext()->GetTexture(splashTexture);
-	ImGui::Image(reinterpret_cast<ImTextureID>(textureHandle.idx), ImVec2(500, 300));
+	ImGui::Image(ImTextureID(textureHandle.idx), ImVec2(400, 300));
+	ImGui::Spacing();
+	ImGui::Separator();
+	ImGui::Text("%s", title.c_str());
+	ImGui::Separator();
 
 	ImGui::End();
 }
