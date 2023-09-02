af636f6d5301f5c471c6799b64e17aca0acb9cdb
Hinageshi <69386319+Hinageshi01@users.noreply.github.com>
2023-09-02 22:28:59 +0800

    FileWatcher (#372)
    * FileWatcher

--- Engine/Source/Editor/EditorApp.cpp
+++ Engine/Source/Editor/EditorApp.cpp
@@ -26,6 +26,7 @@
 #include "Rendering/TerrainRenderer.h"
 #include "Rendering/WorldRenderer.h"
 #include "Rendering/ParticleRenderer.h"
+#include "Resources/FileWatcher.h"
 #include "Resources/ResourceBuilder.h"
 #include "Resources/ShaderBuilder.h"
 #include "Resources/ShaderLoader.h"
@@ -74,7 +75,6 @@ void EditorApp::Init(engine::EngineInitArgs initArgs)
 		CD_ERROR("Failed to open CSV file");
 	}
 
-
 	// Phase 1 - Splash
 	//		* Compile uber shader permutations automatically when initialization or detect changes
 	//		* Show compile progresses so it still needs to update ui
@@ -99,10 +99,12 @@ void EditorApp::Init(engine::EngineInitArgs initArgs)
 	m_pEditorImGuiContext->AddStaticLayer(std::make_unique<Splash>("Splash"));
 
 	std::thread resourceThread([]()
-		{
-			ResourceBuilder::Get().Update(true/*doPrintLog*/);
-		});
+	{
+		ResourceBuilder::Get().Update(true/*doPrintLog*/);
+	});
 	resourceThread.detach();
+
+	m_pFileWatcher = std::make_unique<FileWatcher>(CDENGINE_BUILTIN_SHADER_PATH);
 }
 
 void EditorApp::Shutdown()
