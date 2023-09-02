68bfe65b60e3839df607c8ddf7be3dba1f883bfd
Jiajun <75730859+T-rvw@users.noreply.github.com>
2023-09-02 19:38:15 +0800

    add node editor test (#376)

---
+++ Engine/Source/Editor/UILayers/TestNodeEditor.h
@@ -0,0 +1,26 @@
+#include "ImGui/ImGuiBaseLayer.h"
+
+namespace ax::NodeEditor
+{
+
+struct EditorContext;
+
+}
+
+namespace editor
+{
+
+class TestNodeEditor : public engine::ImGuiBaseLayer
+{
+public:
+	using ImGuiBaseLayer::ImGuiBaseLayer;
+	virtual ~TestNodeEditor();
+
+	virtual void Init() override;
+	virtual void Update() override;
+
+private:
+	ax::NodeEditor::EditorContext* m_pNodeEditorContext = nullptr;
+};
+
+}
