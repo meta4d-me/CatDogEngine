7653cbf172c53872b9cf28c60b032eef2140bb24
VTui22 <zw18627636515@163.com>
2023-09-02 18:02:31 +0800

    update

--- Engine/Source/Runtime/Rendering/SkeletonRenderer.cpp
+++ Engine/Source/Runtime/Rendering/SkeletonRenderer.cpp
@@ -11,26 +11,39 @@ namespace engine
 
 namespace details
 {
-void TraverseBone(const cd::Bone& bone, const cd::SceneDatabase* pSceneDatabase, std::byte* currentDataPtr, 
-	std::byte* currentIndexPtr, uint32_t indexTypeSize, uint32_t currentDataSize, uint32_t currentIndexSize)
+
+cd::Vec3f CalculateBoneTranslate(const cd::Bone& bone,cd::Vec3f& translate, const cd::SceneDatabase* pSceneDatabase)
+{
+	const cd::Bone& parentBone = pSceneDatabase->GetBone(bone.GetParentID().Data());
+	translate += parentBone.GetTransform().GetTranslation();
+	if (0U != bone.GetParentID().Data())
+	{
+		CalculateBoneTranslate(parentBone, translate, pSceneDatabase);
+	}
+	return translate;
+}
+void TraverseBone(const cd::Bone& bone, const cd::SceneDatabase* pSceneDatabase, std::byte* currentDataPtr,
+	std::byte* currentIndexPtr, size_t indexTypeSize, uint32_t& currentDataSize, uint32_t& currentIndexSize)
 {
 	constexpr uint32_t posDataSize = cd::Point::Size * sizeof(cd::Point::ValueType);
-
 	for (auto& child : bone.GetChildIDs())
 	{
 		const cd::Bone& currBone = pSceneDatabase->GetBone(child.Data());
 		const cd::Bone& parent = pSceneDatabase->GetBone(currBone.GetParentID().Data());
-		const cd::Point& position = currBone.GetOffset().GetTranslation();
-		uint32_t parentID = currBone.GetParentID().Data();
-		uint32_t currBoneID = currBone.GetID().Data();
-		std::memcpy(&currentDataPtr[currentDataSize], position.Begin(), posDataSize);
+		cd::Vec3f translate = currBone.GetOffset().GetTranslation();
+
+		//const cd::Vec3f position = details::CalculateBoneTranslate(currBone, translate, pSceneDatabase);
+
+		uint16_t parentID = currBone.GetParentID().Data();
+		uint16_t currBoneID = currBone.GetID().Data();
+		std::memcpy(&currentDataPtr[currentDataSize], translate.Begin(), posDataSize);
+		currentDataSize += posDataSize;
 		std::memcpy(&currentIndexPtr[currentIndexSize], &parentID, indexTypeSize);
 		currentIndexSize += static_cast<uint32_t>(indexTypeSize);
 		std::memcpy(&currentIndexPtr[currentIndexSize], &currBoneID, indexTypeSize);
 		currentIndexSize += static_cast<uint32_t>(indexTypeSize);
 
-		currentDataSize += posDataSize;
-		TraverseBone(currBone, pSceneDatabase, currentDataPtr, currentIndexPtr, indexTypeSize, currentDataSize, currentIndexSize );
+		TraverseBone(currBone, pSceneDatabase, currentDataPtr, currentIndexPtr, indexTypeSize, currentDataSize, currentIndexSize);
 	}
 }
 }
@@ -46,33 +59,35 @@ void SkeletonRenderer::UpdateView(const float* pViewMatrix, const float* pProjec
 	bgfx::setViewTransform(GetViewID(), pViewMatrix, pProjectionMatrix);
 }
 
-void TraverseBone(const cd::Bone& bone, const cd::SceneDatabase* pSceneDatabase, std::byte* currentDataPtr,
-	std::byte* currentIndexPtr, size_t indexTypeSize, uint32_t &currentDataSize, uint32_t &currentIndexSize , std::vector<uint16_t>& m_index ,int &index)
+void SkeletonRenderer::Build()
 {
-	constexpr uint32_t posDataSize = cd::Point::Size * sizeof(cd::Point::ValueType);
+	const cd::SceneDatabase* pSceneDatabase = m_pCurrentSceneWorld->GetSceneDatabase();
+	const cd::Bone& firstBone = pSceneDatabase->GetBone(0);
 
-	for (auto& child : bone.GetChildIDs())
-	{
-		const cd::Bone& currBone = pSceneDatabase->GetBone(child.Data());
-		const cd::Bone& parent = pSceneDatabase->GetBone(currBone.GetParentID().Data());
-		const cd::Point& position = currBone.GetOffset().GetTranslation();
-		uint16_t parentID = currBone.GetParentID().Data();
-		uint16_t currBoneID = currBone.GetID().Data();
-		std::memcpy(&currentDataPtr[currentDataSize], position.Begin(), posDataSize);
-		currentDataSize += posDataSize;
-		std::memcpy(&currentIndexPtr[currentIndexSize], &parentID, indexTypeSize);
-		currentIndexSize += static_cast<uint32_t>(indexTypeSize);
-		std::memcpy(&currentIndexPtr[currentIndexSize], &currBoneID, indexTypeSize);
-		m_index[index] = parentID;
-		index++;
-		m_index[index] = currBoneID;
-		index++;
-		currentIndexSize += static_cast<uint32_t>(indexTypeSize);
-	
-		TraverseBone(currBone, pSceneDatabase, currentDataPtr, currentIndexPtr, indexTypeSize, currentDataSize, currentIndexSize, m_index, index);
-		
-	}
-	int temp = 0;
+	const uint32_t vertexCount = pSceneDatabase->GetBoneCount();
+
+	bgfx::setTransform(cd::Matrix4x4::Identity().Begin());
+	cd::VertexFormat vertexFormat;
+	vertexFormat.AddAttributeLayout(cd::VertexAttributeType::Position, cd::AttributeValueType::Float, 3);
+
+	size_t indexTypeSize = sizeof(uint16_t);
+	m_indexBuffer.resize((vertexCount - 1) * 2 * indexTypeSize);
+	m_vertexBuffer.resize(vertexCount * vertexFormat.GetStride());
+	uint32_t currentDataSize = 0U;
+	uint32_t currentIndexSize = 0U;
+	auto currentDataPtr = m_vertexBuffer.data();
+	auto currentIndexPtr = m_indexBuffer.data();
+	constexpr uint32_t posDataSize = cd::Point::Size * sizeof(cd::Point::ValueType);
+	const cd::Point& position = firstBone.GetTransform().GetTranslation();
+	std::memcpy(&currentDataPtr[currentDataSize], position.Begin(), posDataSize);
+	currentDataSize += posDataSize;
+
+	details::TraverseBone(firstBone, pSceneDatabase, m_vertexBuffer.data(), m_indexBuffer.data(), indexTypeSize, currentDataSize, currentIndexSize);
+	bgfx::VertexLayout vertexLayout;
+	VertexLayoutUtility::CreateVertexLayout(vertexLayout, vertexFormat.GetVertexLayout());
+	m_boneVBH = bgfx::createVertexBuffer(bgfx::makeRef(m_vertexBuffer.data(), static_cast<uint32_t>(m_vertexBuffer.size())), vertexLayout).idx;
+	m_boneIBH = bgfx::createIndexBuffer(bgfx::makeRef(m_indexBuffer.data(), static_cast<uint32_t>(m_indexBuffer.size())), 0U).idx;
+
 }
 
 void SkeletonRenderer::Render(float delataTime)
@@ -84,36 +99,11 @@ void SkeletonRenderer::Render(float delataTime)
 		{
 			continue;
 		}
-	
-		const cd::SceneDatabase* pSceneDatabase = m_pCurrentSceneWorld->GetSceneDatabase();
-		const cd::Bone& firstBone = pSceneDatabase->GetBone(0);
-
-		const uint32_t vertexCount = pSceneDatabase->GetBoneCount();
-
-		bgfx::setTransform(cd::Matrix4x4::Identity().Begin());
-		cd::VertexFormat vertexFormat;
-		vertexFormat.AddAttributeLayout(cd::VertexAttributeType::Position, cd::AttributeValueType::Float, 3);
-
-		bool useU16Index = vertexCount <= static_cast<uint32_t>(std::numeric_limits<uint16_t>::max()) + 1U;
-		size_t indexTypeSize = sizeof(uint16_t);
-		m_indexBuffer.resize((vertexCount - 1) * 2 * indexTypeSize);
-		m_index.resize((vertexCount - 1) * 2);
-		m_vertexBuffer.resize(vertexCount * vertexFormat.GetStride());
-		uint32_t currentDataSize = 0U;
-		uint32_t currentIndexSize = 0U;
-		auto currentDataPtr = m_vertexBuffer.data();
-		auto currentIndexPtr = m_indexBuffer.data();
-		constexpr uint32_t posDataSize = cd::Point::Size * sizeof(cd::Point::ValueType);
-		const cd::Point& position = firstBone.GetOffset().GetTranslation();
-		std::memcpy(&currentDataPtr[currentDataSize], position.Begin(), posDataSize);
-		currentDataSize += posDataSize;
-		int index = 0;
-		TraverseBone(firstBone, pSceneDatabase, m_vertexBuffer.data(), m_indexBuffer.data(), indexTypeSize, currentDataSize, currentIndexSize , m_index,index);
-
-		bgfx::VertexLayout vertexLayout;
-		VertexLayoutUtility::CreateVertexLayout(vertexLayout, vertexFormat.GetVertexLayout());
-		m_boneVBH = bgfx::createVertexBuffer(bgfx::makeRef(m_vertexBuffer.data(), static_cast<uint32_t>(m_vertexBuffer.size())), vertexLayout).idx;
-		m_boneIBH = bgfx::createIndexBuffer(bgfx::makeRef(m_indexBuffer.data(), static_cast<uint32_t>(m_indexBuffer.size())), 0U).idx;
+		if (!hasBuilt)
+		{
+			Build();
+			hasBuilt = true;
+		}
 
 		bgfx::setVertexBuffer(0, bgfx::VertexBufferHandle{ m_boneVBH });
 		bgfx::setIndexBuffer(bgfx::IndexBufferHandle{ m_boneIBH });
@@ -125,6 +115,6 @@ void SkeletonRenderer::Render(float delataTime)
 		constexpr StringCrc SkeletonProgram("SkeletonProgram");
 		bgfx::submit(GetViewID(), GetRenderContext()->GetProgram(SkeletonProgram));
 	}
-
+	
 }
 }
