aa30b0e0ea8be77380e9c9355adbfb7c55779b7a
Jiajun <75730859+T-rvw@users.noreply.github.com>
2023-09-23 23:06:24 +0800

    add imgui enum property util (#400)

--- Engine/Source/Editor/UILayers/Inspector.cpp
+++ Engine/Source/Editor/UILayers/Inspector.cpp
@@ -187,7 +187,7 @@ void UpdateComponentWidget<engine::MaterialComponent>(engine::SceneWorld* pScene
 			{
 				// TODO : generic cull mode.
 				ImGuiUtils::ImGuiBoolProperty("TwoSided", pMaterialComponent->GetTwoSided());
-				ImGuiUtils::ImGuiStringProperty("BlendMode", nameof::nameof_enum(pMaterialComponent->GetBlendMode()).data());
+				ImGuiUtils::ImGuiEnumProperty("BlendMode", pMaterialComponent->GetBlendMode());
 			}
 
 			ImGui::Separator();
@@ -200,7 +200,7 @@ void UpdateComponentWidget<engine::MaterialComponent>(engine::SceneWorld* pScene
 		}
 
 		// Textures
-		for (int textureTypeValue = 0; textureTypeValue < static_cast<int>(cd::MaterialTextureType::Count); ++textureTypeValue)
+		for (int textureTypeValue = 0; textureTypeValue < nameof::enum_count<cd::MaterialTextureType>(); ++textureTypeValue)
 		{
 			auto textureType = static_cast<cd::MaterialTextureType>(textureTypeValue);
 			bool allowNoTextures = textureType == cd::MaterialTextureType::BaseColor ||
@@ -386,7 +386,7 @@ void UpdateComponentWidget<engine::LightComponent>(engine::SceneWorld* pSceneWor
 	if (isOpen)
 	{
 		cd::LightType lightType = pLightComponent->GetType();
-		std::string lightTypeName = cd::GetLightTypeName(lightType);
+		std::string lightTypeName(nameof::nameof_enum(lightType));
 
 		ImGuiUtils::ImGuiStringProperty("Type", lightTypeName);
 		ImGuiUtils::ColorPickerProperty("Color", pLightComponent->GetColor());
@@ -559,25 +559,11 @@ void UpdateComponentWidget<engine::SkyComponent>(engine::SceneWorld* pSceneWorld
 
 		if (!skyTypes.empty())
 		{
-			static const char* crtItem = nameof::nameof_enum(engine::SkyType::SkyBox).data();
-			if (ImGui::BeginCombo("##combo", crtItem))
+			auto currentSkyType = pSkyComponent->GetSkyType();
+			if (ImGuiUtils::ImGuiEnumProperty("SkyType", currentSkyType))
 			{
-				for (size_t index = 0; index < skyTypes.size(); ++index)
-				{
-					bool isSelected = (crtItem == skyTypes[index]);
-					if (ImGui::Selectable(skyTypes[index], isSelected))
-					{
-						crtItem = skyTypes[index];
-						pSkyComponent->SetSkyType(static_cast<engine::SkyType>(index));
-					}
-					if (isSelected)
-					{
-						ImGui::SetItemDefaultFocus();
-					}
-				}
-				ImGui::EndCombo();
+				pSkyComponent->SetSkyType(currentSkyType);
 			}
-
 		}
 
 		if (pSkyComponent->GetAtmophericScatteringEnable())
