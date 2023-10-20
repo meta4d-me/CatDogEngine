c873b6fc18be690956a1936ccaf701090cb4726d
t-rvw <429601557@qq.com>
2023-10-21 04:44:55 +0800

    add tone mapping modes

--- Engine/Source/Runtime/Rendering/PostProcessRenderer.h
+++ Engine/Source/Runtime/Rendering/PostProcessRenderer.h
@@ -6,23 +6,20 @@
 namespace engine
 {
 
-	class PostProcessRenderer final : public Renderer
-	{
-	public:
-		using Renderer::Renderer;
-		virtual ~PostProcessRenderer();
+class PostProcessRenderer final : public Renderer
+{
+public:
+	using Renderer::Renderer;
+	virtual ~PostProcessRenderer();
 
-		virtual void Init() override;
-		virtual void UpdateView(const float* pViewMatrix, const float* pProjectionMatrix) override;
-		virtual void Render(float deltaTime) override;
+	virtual void Init() override;
+	virtual void UpdateView(const float* pViewMatrix, const float* pProjectionMatrix) override;
+	virtual void Render(float deltaTime) override;
 
-		virtual void SetEnable(bool value) override;
-		virtual bool IsEnable() const override;
+	void SetSceneWorld(SceneWorld* pSceneWorld) { m_pCurrentSceneWorld = pSceneWorld; }
 
-		void SetSceneWorld(SceneWorld* pSceneWorld) { m_pCurrentSceneWorld = pSceneWorld; }
-
-	private:
-		SceneWorld* m_pCurrentSceneWorld = nullptr;
-	};
+private:
+	SceneWorld* m_pCurrentSceneWorld = nullptr;
+};
 
 }
