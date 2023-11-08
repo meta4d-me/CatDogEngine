b86cb7d7bd5d127cc88325ddffd67aaf824cc361
Hinageshi <2247001665@qq.com>
2023-11-08 11:01:21 +0800

    Code style

--- Engine/Source/Runtime/Rendering/ShaderCollections.h
+++ Engine/Source/Runtime/Rendering/ShaderCollections.h
@@ -44,8 +44,8 @@ public:
 	const std::map<uint32_t, std::set<std::string>>& GetShaderPrograms() const { return m_shaderPrograms; }
 
 	void SetProgramFeatureCombines(std::map<uint32_t, std::set<std::string>> combines);
-	std::map<uint32_t, std::set<std::string>> &GetProgramFeatureCombines() { return m_programFeatureCombines; }
-	const std::map<uint32_t, std::set<std::string>> &GetProgramFeatureCombiness() const { return m_programFeatureCombines; }
+	std::map<uint32_t, std::set<std::string>>& GetProgramFeatureCombines() { return m_programFeatureCombines; }
+	const std::map<uint32_t, std::set<std::string>>& GetProgramFeatureCombiness() const { return m_programFeatureCombines; }
 
 private:
 	// Key : StringCrc(Program name), Value : Shader names
