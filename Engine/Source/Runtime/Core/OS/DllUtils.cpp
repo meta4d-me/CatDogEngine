30b174542aa9306a7ca3b43c0963728bb118befc
T-rvw <429601557@qq.com>
2023-11-10 17:31:44 +0800

    wrap sdl dll helper

---
+++ Engine/Source/Runtime/Core/OS/DllUtils.cpp
@@ -0,0 +1,23 @@
+#include "DllUtils.h"
+
+#include "SDL.h"
+
+namespace engine
+{
+
+void* DllUtils::LoadDll(const char* pFilePath)
+{
+	return SDL_LoadObject(pFilePath);
+}
+
+void* DllUtils::LoadDllFunction(void* pHandle, const char* pFunctionName)
+{
+	return SDL_LoadFunction(pHandle, pFunctionName);
+}
+
+void DllUtils::UnloadDll(void* pHandle)
+{
+	return SDL_UnloadObject(pHandle);
+}
+
+}
