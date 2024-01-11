6a4105f9d2bd9626d3a545226e3c994955dbfa3e
Jiajun <75730859+T-rvw@users.noreply.github.com>
2024-01-11 16:12:27 +0800

    remove vs2019 ci and adjust 3rd build process to fix ci (#450)

--- MakeThirdParty_vs2022.bat
+++ MakeThirdParty_vs2022.bat
@@ -7,4 +7,10 @@ Set CMAKE_IDE_FULL_NAME="Visual Studio 17 2022"
 rem VS_VERSION will be used inside assimp makefile
 Set VS_VERSION=vs2022
 
-call ./Engine/Auto/MakeThirdParty_Windows.bat
+Set CD_BUILD_TYPE=Debug
+call "./Engine/Auto/MakeThirdParty_Windows.bat"
+
+Set CD_BUILD_TYPE=Release
+call "./Engine/Auto/MakeThirdParty_Windows.bat"
+
+pause
